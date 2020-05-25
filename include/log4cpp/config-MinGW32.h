36b235dfd73f84b26c902968b8fffd0f8bd2fe6d
aponysos <aponysos@163.com>
2020-05-25 10:10:49 +0800

    -DLOG4CPP_HAVE_INT64_T -w

--- include/log4cpp/config-MinGW32.h
+++ include/log4cpp/config-MinGW32.h
@@ -25,6 +25,8 @@
 #ifndef LOG4CPP_HAVE_INT64_T
 #define LOG4CPP_HAVE_INT64_T
 #define int64_t __int64
+#endif
+
 
 /* define if the compiler has in_addr_t */
 #ifndef LOG4CPP_HAVE_IN_ADDR_T
@@ -39,8 +41,6 @@ typedef u_long in_addr_t;
 
 /* u_short is the type of sockaddr_in.sin_port */
 // typedef u_short              in_port_t;
-
-#endif
 
 #endif
 
